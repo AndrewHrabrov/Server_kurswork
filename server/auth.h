de4f023a8fd83c61794e44cec1126c6553306e68
AndrewHrabrov <medveddimon623@gmail.com>
2024-12-03 00:18:31 +0300

    Заголовочный файл класса auth

---
+++ server/auth.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <cryptopp/hex.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/cryptlib.h>
+#include <cryptopp/files.h>
+#include <cryptopp/osrng.h>
+#include "UserInterface.h"
+#include "serv_error.h"
+#include "server.h"
+
+#define BUFLEN 5024
+namespace CPP = CryptoPP;
+
+class Auth {
+public:
+    Auth(int socket, std::string file);
+    ~Auth();
+    void authentication(int socket, std::string database);
+
+private:
+    int clientSocket;
+    std::string database;
+    std::string logfile;
+    Logger logger;
+    CryptoPP::HashTransformation*  hash;
+    std::string read_buffer();
+    void sendMessage(int socket, const std::string& message);
+    std::string calculateHash(const std::string& pass, const std::string& salt);
+    void processVectors(int client_socket);
+    std::string getPass(const std::string& filename, const std::string& username);
+};
